530f415f6515e0bed79b2b56b37978c7776e465a
Sven Gastauer <SvenGastauer@users.noreply.github.com>
2020-08-14 09:22:10 +0200

    r vign

---
+++ vignettes/CompareToAnalyticalSolution.R
@@ -0,0 +1,156 @@
+## ----warning=FALSE------------------------------------------------------------
+library(ZooScatR)
+library(ggplot2)
+library(viridis)
+
+## -----------------------------------------------------------------------------
+library(ZooScatR)
+r <- 10 #range 
+a <- 0.01 # radius
+c <- 1477.4 #soundspeed surrounding fluid
+rho <- 1026.8 #density surrounding fluid
+g <- 1028.9/rho #density contrast
+h <- 1480.3/c #soundspeed contrast
+f <- 200 * 1000 #Frequency in Hz
+
+TS.sphere2(f=f,r=r,a=a,c=c,h=h,g=g,rho=rho)
+
+## -----------------------------------------------------------------------------
+r <- 10 #range 
+a <- 0.01 # radius
+c <- 1477.4 #soundspeed surrounding fluid
+rho <- 1026.8 #density surrounding fluid
+g <- 1028.9/rho #density contrast
+h <- 1480.3/c #soundspeed contrast
+
+#Frequency range
+fmin=12
+fmax=400
+freqs <- seq(fmin,fmax, by=1)
+
+## -----------------------------------------------------------------------------
+fname <- paste0(system.file(package="ZooScatR"),"/extdata/configs/config_0.dat") #Loacation of the parameters file
+para = read_para(fname) #Read parameters file
+
+#Create list with soundspeed info
+misc <- list(cw=c) #Set soundspeed in the surrounding fluid
+
+#Set the material properties
+para$phy$g0 <- g #set the density contrast
+para$phy$h0 <- h #set the soundspeed contrast
+
+#set the simulation parameters
+para$simu$var0<-fmin #Set the minimum frequency
+para$simu$var1<-fmax #Set the maximum frequency
+para$simu$n=length(freqs) #Set the number of output points
+para$simu$ni=2000 #any high number
+
+
+## ----warning=FALSE,fig.width=8,fig.height=4-----------------------------------
+#Set the shape parameters
+para$shape$L <- 2*1000*a # 2 * the radius in m to get length in mm
+para$shape$order <- 2 #Set the tapering order
+para$shape$L_a <- 2 #Set L/a
+para$shape$rho_L <- 2000 #any high number to get a high curvature
+
+#Build the sphere and plot the result
+sp <- buildpos(para) #build the shape
+sp$plot #show the shape plot
+
+## ----warning=FALSE,fig.width=8,fig.height=4-----------------------------------
+fs <- as.list(freqs*1000) #Create a list of frequencies
+TS.MJ <- sapply(fs,TS.sphere2,r=r,a=a,c=c,h=h,g=g,rho=rho) #Apply frequency list to the analytical model
+
+## ----warning=FALSE,fig.width=8,fig.height=4-----------------------------------
+#Run DWBA based on config file
+DWBA <- bscat(para=para, misc=misc)
+#Show the model outcome
+DWBA$rplot
+
+
+## ----warning=FALSE,fig.width=8,fig.height=4,tidy=TRUE-------------------------
+#Compare results
+TS.comp<-function(TS1,TS2,freqs){
+  require(ggplot2)
+  require(viridis)
+  TScomp <- as.data.frame(cbind(TS = c(TS1,TS2),
+                 Freq = rep(freqs,2),
+                 Model=rep(c("Analytical","DWBA"),each=length(freqs))))
+  names(TScomp)<- c("TS","Frequency","Model")
+  TScomp$TS <- as.numeric(as.character(TScomp$TS))
+  TScomp$Frequency <- as.numeric(as.character(TScomp$Frequency))
+  
+  err <- sum(abs(TS1-TS2))/length(freqs)
+  TScomp$diff <- rep(abs(TS1-TS2),2)
+  message(paste("Overall error:", round(mean(err),2),"dB"))
+  #ggplot remove backgorund
+  rmb <- theme(axis.line = element_line(colour = "black"),
+    panel.grid.major = element_blank(),
+    panel.grid.minor = element_blank(),
+    panel.border = element_blank(),
+    panel.background = element_blank())
+  
+  print(ggplot(data=TScomp,aes(x=Frequency,y=TS, group=Model,col=Model))+
+          geom_line(lwd=1)+
+          scale_color_brewer(palette="Set1")+
+          theme(legend.position = "top")+
+          xlab("Frequency [kHz]")+
+          rmb)
+  print(ggplot(data=TScomp,aes(x=Frequency,y=diff, col=diff))+
+          geom_line(lwd=1.2)+
+          scale_color_viridis(name="dB difference")+
+          theme(legend.position="top")+xlab("Frequency [kHz]")+rmb)
+  return(TScomp)
+}
+
+## ----warning=FALSE,fig.width=8,fig.height=4-----------------------------------
+TS.compare <- TS.comp(TS.MJ,DWBA$y,freqs)
+
+## -----------------------------------------------------------------------------
+a <- 0.01
+g <- 1.0357 #density contrast
+h <- 1.0279 #soundspeed contrast
+c <- 1456
+
+#Set the material properties in ZooScatR
+para$shape$L <- 2*1000*a # 2 * the radius in m to get length in mm
+para$phy$g0 <- g #set the density contrast
+para$phy$h0 <- h #set the soundspeed contrast
+misc$cw <- c
+
+#Run the analytical model
+TS.MJ <- sapply(fs,TS.sphere2,r=r,a=a,c=c,h=h,g=g,rho=rho) #Apply frequency list to the analytical model
+
+#Run the ZooScatR DWBA:  
+DWBA <- bscat(para=para, misc=misc)
+
+## ----warning=FALSE,fig.width=8,fig.height=4-----------------------------------
+TS.compare <- TS.comp(TS.MJ,DWBA$y,freqs)
+
+## ----warning=FALSE------------------------------------------------------------
+th <- - 80
+tmp1 <- TS.MJ
+tmp1[which(tmp1 < th)] <- th
+tmp2 <- DWBA$y
+tmp2[which(tmp2 < th)] <- th
+Ts.compare2 <- TS.comp(tmp1,tmp2,freqs)
+
+## -----------------------------------------------------------------------------
+a <- 0.0381
+g <- 14900/rho #density contrast
+h <- 6853/c #soundspeed contrast
+
+#Set the material properties in ZooScatR
+para$shape$L <- 2*1000*a # 2 * the radius in m to get length in mm
+para$phy$g0 <- g #set the density contrast
+para$phy$h0 <- h #set the soundspeed contrast
+
+#Run the analytical model
+TS.MJ <- sapply(fs,TS.sphere2,r=r,a=a,c=c,h=h,g=g,rho=rho) #Apply frequency list to the analytical model
+
+#Run the ZooScatR DWBA:  
+DWBA <- bscat(para=para, misc=misc)
+
+## ----warning=FALSE,fig.width=8,fig.height=4-----------------------------------
+TS.compare <- TS.comp(TS.MJ,DWBA$y,freqs)
+
--- vignettes/parameters.R
+++ vignettes/parameters.R
@@ -1,41 +1,3 @@
-## ----setup, echo = FALSE, message = FALSE-------------------------------------
-knitr::opts_chunk$set(collapse = T, comment = "#>")
-options(tibble.print_min = 4L, tibble.print_max = 4L)
-library(ZooScatR)
-set.seed(1014)
-
-## ----fig.width=8,fig.height=4-------------------------------------------------
-fname <- paste0(system.file(package="ZooScatR"),"/extdata/configs/config_0.dat") #Loacation of the parameters file
-para = read_para(fname) #Read parameters file
-#Create list with soundspeed info
-misc <- list(cw=1500)
-#Run DWBA based on config file
-res <- bscat(para=para, misc=misc) #Target strength vs Frequency
-res$rplot #Show the result plot
-#Target strength vs angular position
-para$simu$var_indx <- 2 #Set output variable to Angular position
-#Run DWBA based on config file
-res <- bscat(para=para, misc=misc)
-res$rplot #Show the result plot
-#Backscattering amplitude vs ka (Wavenumber x width)
-para$simu$out_indx <- 1 #Set output to Backscattering amplitude
-para$simu$var_indx <- 3 #Set output variable to ka
-#Run DWBA based on config file
-res <- bscat(para=para, misc=misc)
-res$rplot #Show the result plot
-#Reduced Target Strength vs Frequency (Wavenumber x width)
-para$simu$out_indx <- 4 #Set output to Reduced Target Strength
-para$simu$var_indx <- 1 #Set output variable to Frequency
-#Run DWBA based on config file
-res <- bscat(para=para, misc=misc)
-res$rplot #Show the result plot
-
-## -----------------------------------------------------------------------------
-# filenmae of an example configuration
-fname <- paste0(system.file(package="ZooScatR"),"/extdata/configs/config_0.dat")
-#Read the parameter file
-para <- read_para(fname)
-
 ## ----echo=FALSE---------------------------------------------------------------
 para.df <- as.data.frame(cbind(Variable = c("Length(L)",
                                       "$\\rho_c$/L",
@@ -69,7 +31,7 @@ para.df <- as.data.frame(cbind(Variable = c("Length(L)",
                                          "Filter length to smooth the radius (R)",
                                          "Filter length to smooth the body axis (X, Z)"),
                          Influence = c("Length of the target",
-                                       "Curvature of the bent cylinder, if no shape profile is loaded ( ??? for straight cylinder)",
+                                       "Curvature of the bent cylinder, if no shape profile is loaded ( ∞ for straight cylinder)",
                                        "Contains information about the width of the target.",
                                        "For a straight cylinder ($\\rho_c$/L -> infinity), n=2 results in a prolate spheroid if L>2a, or in an oblate spheroid for L<2a",
                                        "1 = perform average; 0 = No average",
@@ -101,13 +63,13 @@ orient.df <- as.data.frame(cbind(Variable = c("Mean Theta ($\\theta$)",
                                       "Max. Theta ($\\theta_{max}$)",
                                       "Std.($\\theta$)",
                                       "Increment $\\theta$"),
-                         Unit = c("$^\\circ$",
+                         Unit = c("°",
                                   "Boolean",
                                   "distribution (Uniform or Gaussian)",
-                                  "$^\\circ$",
-                                  "$^\\circ$",
-                                  "$^\\circ$",
-                                  "$^\\circ$"),
+                                  "°",
+                                  "°",
+                                  "°",
+                                  "°"),
                          Description = c("Mean orientation angle",
                                          "Defines if an orientation average should be considered",
                                          "PDF of theta",
@@ -193,7 +155,7 @@ simu.df <- as.data.frame(cbind(Variable = c("Output",
                                          "Maximum output value",
                                          "Defines the discrete frequency for which the model is run if the variable is set to angle"),
                          Influence = c("1) backscattering amplitude; 2) differential backscattering cross-section [m2]; 3) Reduced Target Strength [dB re m2]; 4) Reduced Target Strength [db re m2]",
-                                         "1) Frequency [kHz]; 2) Angle ($^\\circ$); 3) ka (wave number k * a)",
+                                         "1) Frequency [kHz]; 2) Angle (°); 3) ka (wave number k * a)",
                                          "Resolution of the model variable output",
                                          "Resolution of the model shape input",
                                          
@@ -210,235 +172,4 @@ simu.df <- as.data.frame(cbind(Variable = c("Output",
                              "...simu$freq")
                          ))
 knitr::kable(simu.df, keep.line.breaks = TRUE, style = 'grid', justify = 'left')
-
-## ----echo=TRUE----------------------------------------------------------------
-#Changing the length of the scattering target
-message(paste("Old length:", para$shape$L))
-para$shape$L <- 40
-message(paste("New length:", para$shape$L))
-#Changing the mean orientation of the scattering target
-message(paste("Old orientation angle:", para$orient$angm))
-para$orient$angm <- 30
-message(paste("New orientation angle:", para$orient$angm))
-#Changing the density contrast
-message(paste("Old density contrast:", para$phy$g0))
-para$phy$g0 <- 1.031
-message(paste("New density contrast:", para$phy$g0))
-#Changing the output start value
-message(paste("Old output start value:", para$simu$var0))
-para$simu$var0 <- 38
-message(paste("New output start value:", para$simu$var0))
-#Changing the output end value
-message(paste("Old output end value:", para$simu$var1))
-para$simu$var0 <- 200
-message(paste("New output end value:", para$simu$var1))
-
-## ----fig.width=6,fig.height=3-------------------------------------------------
-cyl <- buildpos(para)
-message(paste("Length:",para$shape$L))
-message(paste("L/a:",para$shape$L_a))
-message(paste("Tapering order:",para$shape$order))
-cyl$plot
-
-para$shape$order <- 200
-para$shape$L_a <- 3
-cyl <- buildpos(para)
-message(paste("Length:",para$shape$L))
-message(paste("L/a:",para$shape$L_a))
-message(paste("Tapering order:",para$shape$order))
-cyl$plot
-
-para$shape$order <- 2
-para$shape$L_a <- 2
-para$shape$rhoL <- 200
-cyl <- buildpos(para)
-message(paste("Length:",para$shape$L))
-message(paste("L/a:",para$shape$L_a))
-message(paste("Tapering order:",para$shape$order))
-cyl$plot
-
-## ----fig.width=8,fig.height=4-------------------------------------------------
-# filenmae of an example configuration
-fname <- paste0(system.file(package="ZooScatR"),"/extdata/configs/config_0.dat")
-#Read the parameter file
-para <- read_para(fname)
-#set the profile filename
-profname <- paste0(system.file(package="ZooScatR"),"/extdata/profiles/euphaus1.dat") #krill example
-para$shape$prof_name <- profname
-krill_shape1 <- buildpos(para)
-krill_shape1$plot
-
-#change the taper smoothing to some smoothing
-para$shape$taper_sm <- 100
-krill_shape1 <- buildpos(para)
-krill_shape1$plot
-
-#change the axis smoothing, taper smoothing to some extreme values and L/a to obtian a more slender body
-para$shape$L_a <- 30
-para$shape$taper_sm <- 2000
-para$shape$axis_sm <- 2000
-krill_shape1 <- buildpos(para, disp_prof=1)
-krill_shape1$plot
-
-## ---- echo=FALSE--------------------------------------------------------------
-taxa <- c("Euphausiids and Decapod Shrimp",
-          "Larval Crustacean",
-          "Amphipods",
-          "Ostracods",
-          "Chaetognaths and Polychaetes",
-          "Gymnosome Pteropods (Clione)",
-          "Salps",
-          "Copepods",
-          "Medusae",
-          "Eggs",
-          "Calanus finmarchicus",
-          "Calanus hyperboreus",
-          "Acartia clausi",
-          "Calanus marshallae",
-          "Meganyctiphanes norvegica",
-          "Thysanoessa",
-          "Calanus",
-          "General",
-          "Euphausia superba",
-          "Euphausia crystallorophias",
-          "Neocalanus cristatus",
-          "Neocalanus plumchrus",
-          "Ammodytes personatus (juvenile)",
-          "Ammodytes personatus (adult)",
-          "Salpa thompsoni")
-
-La <- c("10.5",
-        "2.55 [@lawson_acoustically-inferred_2004]",
-        "3.00 [@lawson_acoustically-inferred_2004]",
-        2.55,
-        17.15,
-        1.83,
-        4.0,
-        2.55,
-        NA,
-        NA,
-        NA,
-        NA,
-        NA,
-        NA,
-        NA,
-        NA,
-        NA,
-        NA,
-        NA,
-        NA,NA,NA,NA,NA,10)
-
-Orientation <- c("N(20,20) [@benfield_estimating_2000], average ~0 [@lawson_improved_2006]",
-                 "N(0.30) [@lawson_acoustically-inferred_2004]",
-                 "N(0,30) [@lawson_acoustically-inferred_2004]",
-                 "N(0,30)",
-                 "N(0,30)",
-                 "N(0,30)",
-                 "N(0,30)",
-                 "N(90,30)",
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,
-                 NA,"Uniform [0, ??]")
-
-
-g <- c("$$\\frac{5.485*L}{10^4}+1.002; L>25$$ $$1.016; L<25$$ [@lawson_acoustically-inferred_2004]",
-       "1.058 [@lawson_acoustically-inferred_2004]",
-       "1.058 [@lawson_acoustically-inferred_2004]",
-       "1.03 [pers. comm. Dezhang Chu in @lavery_determining_2007]",
-       "1.03 [pers. comm. Dezhang Chu in @lavery_determining_2007]",
-       "1.03 [pers. comm. Dezhang Chu in @lavery_determining_2007]",
-       "1.004 [@stanton_acoustic_1994]",
-       "1.02 [pers. comm. Dezhang Chu in @lavery_determining_2007]",
-       "1.02 [inferred from @monger_sound_1998 as reported in @lavery_determining_2007]",
-       "0.979 [@chu_material_2003]",
-       "1.025 - 1.029  g/cm^3^ depending on season [@kogeler_density-and_1987]",
-       "1.022 - 1.036  g/cm^3^ depending on season [@kogeler_density-and_1987]",
-       "1.04 g/cm^3^ [@greenlaw_acoustical_1979]",
-       "1.04 g/cm^3^ [@greenlaw_acoustical_1979]",
-       "1.057 g/cm^3^ [@kils_swimming_1981]",
-       "1.052 - 1.074 g/cm^3^ depending on season and species [@kogeler_density-and_1987]",
-       "1.022 - 1.036 g/cm^3^ depending on season and species [@kogeler_density-and_1987]",
-       "0.9402 - 1.051 [@chu_measurements_2005]",
-       "1.0241 [@chu_measurements_2005], 1.0357 [@foote_speed_1990]",
-       "1.009 & 1.000 depth dependent [@chu_measurements_2005]",
-       "0.997 - 1.009 [@matsukura_measurements_2009]",
-       "0.995 - 1.009 [@matsukura_measurements_2009]",
-       "1.021 [@yasuma_density_2009]",
-       "1.032 [@yasuma_density_2009]",
-       "1 - 1.0039 [@wiebe_acoustic_2009]"
-       )
-
-
-
-h <- c("$$\\frac{5.94*2L}{10^ 4}; L>25$$ $$1.019; L<25$$  [@lawson_acoustically-inferred_2004]",
-       "1.058 [@lawson_acoustically-inferred_2004]",
-       "1.058 [@lawson_acoustically-inferred_2004]",
-       "1.03 [pers. comm. Dezhang Chu in @lavery_determining_2007]",
-       "1.03 [pers. comm. Dezhang Chu in @lavery_determining_2007]",
-       "1.03 [pers. comm. Dezhang Chu in @lavery_determining_2007]",
-       "1.004 [@stanton_acoustic_1994]",
-       "1.058 [@chu_inference_2000]",
-       "1.02 [inferred from @monger_sound_1998 as reported in @lavery_determining_2007]",
-       "1.017 [@stanton_simple_1989]",
-       "1.027 (mixture C. finmarchicus and C. hyperboreus) [@kogeler_density-and_1987]",
-       "1.027 (mixture C. finmarchicus and C. hyperboreus) [@kogeler_density-and_1987]",
-       "NA",
-       "1.007 [@kogeler_density-and_1987]",
-       "1.030 [@kogeler_density-and_1987]",
-       "1.026 [@kogeler_density-and_1987]",
-       "1.027 [@kogeler_density-and_1987];  0.949 & 1.013 - Depth dependent [@chu_measurements_2005]",
-       "0.949 - 1.096 [@chu_measurements_2005]",
-       "1.031 [@chu_measurements_2005], 1.0279 [@foote_speed_1990]",
-       "1.025 & 1.029 depth dependent [@chu_measurements_2005]",
-       "1.013 - 1.025 [@matsukura_measurements_2009]",
-       "1.006 - 1.021 [@matsukura_measurements_2009]",
-       "1.02 [@yasuma_density_2009]",
-       "1.02 [@yasuma_density_2009]",
-       "1.006 - 1.0201 [@wiebe_acoustic_2009]")
-
-
-model <- c("DWBA uniformly-bent cylinder","DWBA uniformly-bent cylinder","DWBA uniformly-bent cylinder","DWBA uniformly-bent cylinder","DWBA uniformly-bent cylinder","DWBA uniformly-bent cylinder","DWBA uniformly-bent cylinder","DWBA prolate spheroid", "DWBA two prolate spheroid surfaces", "High-pass fluid sphere","NA","NA","NA","NA","NA","NA","NA",NA,NA,NA,"DWBA Deformed Cylinder","DWBA Deformed Cylinder",NA,NA,"DWBA")
-
-gh <- as.data.frame(cbind(taxa,La,Orientation,g,h,model))
-names(gh)<- c("Taxon",
-              "Length-to-girth ratio $\\frac{L}{a}$",
-              "Orientation",
-              "Density Contrast g", 
-              "Sound Speed Contrast h",
-              "Scattering model")
-knitr::kable(gh)
-#datatable(gh)
-
-## -----------------------------------------------------------------------------
-c_Coppens1981(D=100,S=35,T=10)
-
-## -----------------------------------------------------------------------------
-c_Mackenzie1981(D=100,S=35,T=10)
-
-## -----------------------------------------------------------------------------
-c_Leroy08(Z=100,T=10,S=35,30)
-
-## -----------------------------------------------------------------------------
-rho(S=35, T=10)
-#which is equal to:
-rho_p0(S=35,T=10)
-
-## -----------------------------------------------------------------------------
-rho_smow(T=10)
-
-## -----------------------------------------------------------------------------
-result <- bscat(para=para, misc=misc)
 
