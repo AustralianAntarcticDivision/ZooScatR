c6d83d95df6867154aaa466a92f8396122a3ee83
Sven Gastauer <SvenGastauer@users.noreply.github.com>
2022-08-09 18:10:14 +0200

    add 3d plot to shape

--- R/app.R
+++ R/app.R
@@ -1,3 +1,84 @@
+
+#############################################################################
+#############################################################################
+#' plot_3D function to create a 3D plot of a prolate spheroid
+#' @description Creates a 3D plotly plot of the prolate spheroid shape input
+#' for ZooScatR, based on the parameters file.
+#' @param para a list of parameters as used in ZooScatR
+#' @param  nx number of point constituing circular elements
+#' @return a plotly plot object
+#' @export
+#' @import plotly
+#' @examples
+#'
+plot_3D <- function(para,nx=100){
+  #build from parameters
+  ppp <-  buildpos(para=para)
+  #get needed info from parameters
+  L = para$shape$L
+  nz <- para$simu$ni
+  #get positions from buildpos
+  x = ppp$x
+  z = ppp$z
+  #relative radius
+  y=ppp$taper/para$shape$L_a
+
+  #angles for circular elements
+  th <- seq(0, 2 * pi, length = nx)
+
+  #circular elements
+  zi <- rep(z - min(z),each=length(th))
+  xi <- as.vector(t(apply(matrix(th),1,function(o) x + y * cos(o) + 0.1)))
+  yi <- as.vector(t(apply(matrix(th),1,function(o) x + y * sin(o) + 0.1)))
+
+  #element number
+  i =  rep(1:length(ppp$x),each=length(th))
+
+  #aspect ratios
+  xr <- diff(range(zi)/2)
+  yr <- diff(range(xi))/2
+  zr <- diff(range(yi))/2
+  mr <- max(xr,yr,zr)
+
+
+  p<- plot_ly(x = ~zi * L/2, y = ~xi*L, z = ~yi*L, split=~i, type = 'scatter3d', mode = 'lines',
+              opacity = 0.5, line = list(width = 6, reverscale = FALSE,color='black')) %>%
+    layout(scene = list(
+      xaxis= list(title = "X (mm)",nticks = 4),
+      yaxis = list(title = "Y (mm)",nticks = 4),
+      zaxis = list(title = "Z (mm)",nticks = 4),
+      aspectratio = list(x = xr/mr, y = yr/mr, z = zr/mr)),
+      showlegend=FALSE)
+
+  return(p)
+}
+
+
+#' Set number of variable to be computed by resolution
+#' @description Creates a 3D plotly plot of the prolate spheroid shape input
+#' for ZooScatR, based on the parameters file.
+#' @param para a list of parameters as used in ZooScatR
+#' @param  res the resolution of the output variable
+#' @return a plotly plot object
+#' @export
+#'
+set_res<- function(para,res=1){
+  para$simu$n = (para$simu$var1 - para$simu$var0 + res)/ res
+  return(para)
+}
+
+
+#' Compute L/a for given a and L
+#' @description Generates L/a based on a radius and L input
+#' @param para a list of parameters as used in ZooScatR
+#' @param  a maximum radius of the input shape
+#' @return a plotly plot object
+#' @export
+#'
+set_la<- function(para,a=2){
+  para$shape$L_a = (para$shape$L / a)
+  return(para)
+}
 #' DWBA shiny app
 #' @description This is a Shiny web application for the DWBA model.
 #'
@@ -416,7 +497,12 @@ DWBAapp <- function(){
                                              shiny::downloadButton("export","Export as csv"),
                                              shiny::downloadButton("export_sim","Export all as RDS"),
                                              shiny::hr(),
-                                             shiny::plotOutput("shapePlot"),
+                                             shiny::tabsetPanel(id="shplots",
+                                                              shiny::tabPanel("Shape 2D",
+                                                                              shiny::plotOutput("shapePlot")),
+                                                              shiny::tabPanel("Shape 3D",
+                                                                              plotly::plotlyOutput("shapePlot2"))
+                                                              ),
                                              shiny::br(),
                                              plotly::plotlyOutput("resPlot"),
                                              shiny::uiOutput("dynamic"),
@@ -920,6 +1006,9 @@ DWBAapp <- function(){
           #plot shape
           output$shapePlot <- shiny::renderPlot({
             print(res$shplot)
+          })
+          output$shapePlot2 <- plotly::renderPlotly({
+            plot_3D(para)
           })
 
           #plot results
